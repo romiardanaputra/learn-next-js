4bb2300bb6961ed9ce27d8edeca29c9ee9261b1d
romiardanaputra <romiardana21@gmail.com>
2024-07-16 20:10:14 +0800

    feat(Route): Add nested route (docs learning)

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return <div>
+    <h1>blog first</h1>
+  </div>
+}
+
+export default Page
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h1>second blog</h1>
+    </div>
+  );
+};
+
+export default Page;
