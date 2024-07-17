fdc51f04cbe3a44781ec56f4a3f5f3b30cdcf3c5
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2024-07-17 17:09:27 +0800

    [MAIN] Docs/navigation (#10)
    * docs(nav): Create example Link component navigation
    
    * docs(nav): Create example active Link component navigation

--- src/app/(auth)/(auth-layout)/layout.tsx
+++ src/app/(auth)/(auth-layout)/layout.tsx
@@ -1,14 +1,36 @@
+"use client";
 import { NextPage } from "next";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
 }
 
+const navLinks = [
+  { linkName: "register", href: "/register" },
+  { linkName: "login", href: "/login" },
+  { linkName: "forgot password", href: "/forgot-password" },
+];
+
 const AuthLayout: NextPage<Props> = ({ children }) => {
+  const pathName = usePathname();
   return (
     <div>
       <h1>Auth Layout</h1>
       <p>Welcome to page login/register/forgot password</p>
+      {navLinks.map((link, index) => {
+        const isActive = pathName.startsWith(link.href);
+        return (
+          <Link
+            key={index}
+            href={link.href}
+            className={isActive ? "text-yellow-400" : ""}
+          >
+            {link.linkName}
+          </Link>
+        );
+      })}
       {children}
     </div>
   );
