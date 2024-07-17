14fee9a964ae588a6b0c8eca56c4b9d4cebc7950
romiardanaputra <romiardana21@gmail.com>
2024-07-17 15:58:18 +0800

    docs(route): Create example of dynamic route in products details page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,24 @@
 import { NextPage } from "next";
-
+import { Metadata } from "next";
 interface Props {
   params: {
     productId: string;
   };
 }
+
+// exmple generate dynamic metadata
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(`Manga ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
 
 const ProductDetail: NextPage<Props> = ({ params }) => {
   return (
