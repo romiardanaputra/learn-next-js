14f2df5066fac22d5d0c09719cea9abb1326f5ac
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2024-07-18 12:01:56 +0800

    [MAIN] docs(template): Create example template usage (#11)

--- src/app/(auth)/(auth-layout)/layout.tsx
+++ src/app/(auth)/(auth-layout)/layout.tsx
@@ -2,6 +2,7 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -15,9 +16,18 @@ const navLinks = [
 
 const AuthLayout: NextPage<Props> = ({ children }) => {
   const pathName = usePathname();
+  const [input, setInput] = useState("");
   return (
     <div>
       <h1>Auth Layout</h1>
+      <input
+        type="text"
+        name="email"
+        placeholder="jondoe@gmail.com"
+        className="text-black"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
       <p>Welcome to page login/register/forgot password</p>
       {navLinks.map((link, index) => {
         const isActive = pathName.startsWith(link.href);
