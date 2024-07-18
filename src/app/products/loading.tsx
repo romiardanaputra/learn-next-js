b17e06a473c4cfb7b8e6aa1a08935d0a5679d232
romiardanaputra <romiardana21@gmail.com>
2024-07-18 12:14:38 +0800

    docs(loading): Create example usage of special page called loading

---
+++ src/app/products/loading.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const ProductLoading: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h1>loading product...</h1>
+    </div>
+  );
+};
+
+export default ProductLoading;
