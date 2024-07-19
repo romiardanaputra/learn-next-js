a98df7e8b009fe0557af462b8b2447e108baaed6
romiardanaputra <romiardana21@gmail.com>
2024-07-19 18:29:32 +0800

    docs(route): Create simple example of route interception

---
+++ src/app/folder1/folder2/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Folder2Example: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h1>Example of folder 2 intercepting route</h1>
+    </div>
+  );
+};
+
+export default Folder2Example;
